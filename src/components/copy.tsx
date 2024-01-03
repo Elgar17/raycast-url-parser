dc4e03fbd021960d23d3726f119b2f9b643ed88c
Elgar17 <1653794708@qq.com>
2024-01-03 21:59:45 +0800

    fix: lint

--- src/components/copy.tsx
+++ src/components/copy.tsx
@@ -1,13 +1,11 @@
-import { Icon, ActionPanel, Action } from '@raycast/api'
+import { Icon, ActionPanel, Action } from "@raycast/api";
 
 const Copy = (ctx: string) => {
-    return <ActionPanel>
-        <Action.CopyToClipboard
-            title="Copy"
-            content={ctx}
-            icon={Icon.CopyClipboard}
-        />
+  return (
+    <ActionPanel>
+      <Action.CopyToClipboard title="Copy" content={ctx} icon={Icon.CopyClipboard} />
     </ActionPanel>
-}
+  );
+};
 
-export default Copy
+export default Copy;
